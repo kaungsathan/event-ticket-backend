0feaf31a9e31b6a591839bbe14f940ce4d4e9fd6
Kaungsat Han <kaungsathan@onenex.co>
2025-09-05 10:21:19 +0630

    add auth event create

--- app/Domains/Events/Routes/events.php
+++ app/Domains/Events/Routes/events.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'events'], function () {
     Route::get('/', [EventController::class, 'index']);
-    Route::post('/', [EventController::class, 'store']);
+    Route::post('/', [EventController::class, 'store'])->middleware('auth:sanctum');
     Route::get('/{id}', [EventController::class, 'show']);
     Route::post('/{id}/update', [EventController::class, 'update']);
     Route::delete('/{id}', [EventController::class, 'destroy']);
