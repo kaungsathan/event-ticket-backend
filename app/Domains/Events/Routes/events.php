ed6b3954043d086ae80f39053b8adcd93a64f16e
Kaungsat Han <kaungsathan@onenex.co>
2025-09-05 11:12:52 +0630

    fix auth

--- app/Domains/Events/Routes/events.php
+++ app/Domains/Events/Routes/events.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'events'], function () {
     Route::get('/', [EventController::class, 'index']);
     Route::post('/', [EventController::class, 'store'])->middleware('auth:sanctum');
-    Route::get('/{id}', [EventController::class, 'show']);
-    Route::post('/{id}/update', [EventController::class, 'update']);
-    Route::delete('/{id}', [EventController::class, 'destroy']);
+    Route::get('/{id}', [EventController::class, 'show'])->middleware('auth:sanctum');
+    Route::post('/{id}/update', [EventController::class, 'update'])->middleware('auth:sanctum');
+    Route::delete('/{id}', [EventController::class, 'destroy'])->middleware('auth:sanctum');
 });
